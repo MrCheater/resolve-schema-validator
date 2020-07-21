1f0c19fca7514c19cb242d5334063899772a56f6
timbset <timbset@gmail.com>
2020-07-21 11:11:11 +0300

    Add comments with validation explaining

--- src/handler.ts
+++ src/handler.ts
@@ -9,6 +9,16 @@ import validate from "./utils/validate";
 import getItem from "./get-item";
 import setItem from "./set-item";
 
+/*
+
+Basic rules:
+1. Lambda Event has the 'unknown' type
+2. No unions - validate specific type (primitive or object in specific switch branch).
+   It helps to prevent weird error messages.
+3. Validation function gets unknown value and returns value with casting type (or throws error)
+4. All validations have to be done as high as possible in code (basically in handler)
+
+*/
 async function handler(lambdaEvent: unknown): Promise<any> {
   const event = validate(EventSchema, lambdaEvent);
   switch (event.method) {
